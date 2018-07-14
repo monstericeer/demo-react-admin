62d551756676cfa8d24471c5b7ba45b60c8dd3c7
mochen <mochen0505@hotmail.com>
2018-07-15 02:40:46 +0800

    minor change in UI

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import dizzy from '../assets/svgs/dizzy.svg';
 import '../assets/css/noMatch.less';
 
 class NoMatch extends React.Component {
     render() {
         return (
-            <div className='no_match'>404 页面不存在</div>
+            <div className='no_match'>
+                <embed className='no_match_font' src={dizzy}/>
+                <div className='no_match_text'>404 页面不存在</div>
+            </div>
         )
     }
 }
